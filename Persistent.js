a11c15e0a5a886db902faae8445dfeb0439c4c50
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-10-21 20:53:19 -0700

    Update Persistent.js

--- Persistent.js
+++ Persistent.js
@@ -23,3 +23,10 @@ function persistence(num) {
 }
 
 // or
+
+function persistence(num) {
+  for (let i = 0; num > 9; i++) {
+    num = num.toString().split('').reduce((t, c) => c * t);
+  }
+  return i;
+}
